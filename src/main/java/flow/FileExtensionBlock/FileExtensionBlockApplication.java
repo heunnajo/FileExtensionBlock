dba07d3e35bd081776b731f2601aec8035f437d7
heunnajo <61869540+heunnajo@users.noreply.github.com>
2023-02-07 11:01:11 +0900

    Initial commit

---
+++ src/main/java/flow/FileExtensionBlock/FileExtensionBlockApplication.java
@@ -0,0 +1,18 @@
+package flow.FileExtensionBlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileExtensionBlockApplication {
+
+	public static void main(String[] args) {
+//		Hello h = new Hello();
+//		h.setData("Hello World");
+//		String data = h.getData();
+//		System.out.println("data: "+data);
+
+		SpringApplication.run(FileExtensionBlockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flow/FileExtensionBlock/FileExtensionBlockApplicationTests.java
@@ -0,0 +1,13 @@
+package flow.FileExtensionBlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileExtensionBlockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
