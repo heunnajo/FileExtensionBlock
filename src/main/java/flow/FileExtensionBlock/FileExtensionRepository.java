1ee9516ac98d1fefa594e5b8a66a648c2eecca6e
heunnajo <61869540+heunnajo@users.noreply.github.com>
2023-02-07 16:50:47 +0900

    Update FileExtensionRepository.java
    레포지토리 계층 개발

--- src/main/java/flow/FileExtensionBlock/FileExtensionRepository.java
+++ src/main/java/flow/FileExtensionBlock/FileExtensionRepository.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Repository
 public class FileExtensionRepository {
@@ -18,5 +22,25 @@ public class FileExtensionRepository {
 
     public FileExtension find(Long id){
         return em.find(FileExtension.class,id);
+    }
+
+    public Optional<FileExtension> findById(Long id) {
+        FileExtension fe = em.find(FileExtension.class, id);
+        return Optional.ofNullable(fe);
+    }
+    public List<FileExtension> findAll(){
+        return em.createQuery("select m from FileExtension m", FileExtension.class)
+                .getResultList();
+    }
+
+    public Optional<FileExtension> findByName(String name) {
+        List<FileExtension> result = em.createQuery("select m from FileExtension m where m.name = :name", FileExtension.class)
+                .setParameter("name", name)
+                .getResultList();
+        return result.stream().findAny();
+    }
+
+    public void deleteAll(){
+        em.clear();
     }
 }
