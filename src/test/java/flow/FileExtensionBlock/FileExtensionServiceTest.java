d8123275c0cabf7e26a28d45bb47bf74291ea4d5
heunnajo <61869540+heunnajo@users.noreply.github.com>
2023-02-08 09:21:03 +0900

    Update FileExtensionServiceTest.java
    서비스 계층_확장자명취소 테스트 코드 작성

--- src/test/java/flow/FileExtensionBlock/FileExtensionServiceTest.java
+++ src/test/java/flow/FileExtensionBlock/FileExtensionServiceTest.java
@@ -58,4 +58,21 @@ class FileExtensionServiceTest {
                 () -> fileExtensionService.insert(fe2));//예외가 발생해야 한다.
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 확장자명입니다.");
     }
+
+    @Test
+    public void 확장자명_취소() throws Exception{
+        //Given
+        FileExtension fe1 = new FileExtension();
+
+        //When
+        fileExtensionService.delete(fe1.getId());
+
+        //Then : 취소 확인
+        String name = fe1.getExtensionName();
+        if(!fe1.getFixedList().contains(name) && !fe1.getCustomedList().contains(name)){
+            System.out.println("삭제되었습니다!");
+        } else{//오류가 발생한 것임!
+            System.out.println("이 확장자는 삭제되어아하는데 삭제되지 않았습니다!");
+        }
+    }
 }
