8c18603ded00342d0d10fe6a72eb52d985168b0c
heunnajo <61869540+heunnajo@users.noreply.github.com>
2023-02-08 15:52:45 +0900

    Update FileExtension.java
    FileExtension 이라는 엔티티 안에 String 타입의 컬렉션(고정 확장자, 커스텀 확장자)을 저장하고자 했으나 hibernate MappingException 해결하지 못하여 잠시 주석처리.

--- src/main/java/flow/FileExtensionBlock/FileExtension.java
+++ src/main/java/flow/FileExtensionBlock/FileExtension.java
@@ -3,9 +3,7 @@ package flow.FileExtensionBlock;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,20 +16,25 @@ public class FileExtension {
     private String extensionName;
     private boolean isFixed;
 
-    private List<String> fixedList = new ArrayList<>();
-    private List<String> customedList = new ArrayList<>();
-
-    /* 비즈니스 로직 */
-    public void addFixed(String extensionName){
-        fixedList.add(extensionName);
-    }
-    public void removeFixed(String extensionName){
-        fixedList.remove(extensionName);
-    }
-    public void addCustomed(String extensionName){
-        customedList.add(extensionName);
-    }
-    public void removeCustomed(String extensionName){
-        customedList.remove(extensionName);
-    }
+//    @OrderColumn(name = "fixed_idx")
+//    @Column(name = "fixed")
+//    private List<String> fixedList = new ArrayList<>();
+//
+//    @OrderColumn(name = "customed_idx")
+//    @Column(name = "customed")
+//    private List<String> customedList = new ArrayList<>();
+//
+//    /* 비즈니스 로직 */
+//    public void addFixed(String extensionName){
+//        fixedList.add(extensionName);
+//    }
+//    public void removeFixed(String extensionName){
+//        fixedList.remove(extensionName);
+//    }
+//    public void addCustomed(String extensionName){
+//        customedList.add(extensionName);
+//    }
+//    public void removeCustomed(String extensionName){
+//        customedList.remove(extensionName);
+//    }
 }
